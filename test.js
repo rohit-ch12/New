ae517c13c8bd697fe98c422477747b39cc42b34c
spb-superstruct <andrei@superstruct.tech>
2020-01-11 22:51:09 -0800

    feat: add user-agent endpoint

--- test.js
+++ test.js
@@ -9,7 +9,6 @@ const urlBase = `http://localhost:${PORT}`
 tape('should respond hello', (t) => {
   jsonist.get(urlBase, (err, body) => {
     if (err) t.error(err)
-
     t.equal(body.msg, 'hello')
     t.end()
   })
@@ -18,8 +17,16 @@ tape('should respond hello', (t) => {
 tape('should respond b64', (t) => {
   jsonist.get(`${urlBase}/b64/hello`, (err, body) => {
     if (err) t.error(err)
-
     t.equal(body.b64, 'aGVsbG8=')
+    t.end()
+  })
+})
+
+tape('should respond user-agent', (t) => {
+  const opts = { headers: { 'User-Agent': 'tape' } }
+  jsonist.get(`${urlBase}/user-agent`, opts, (err, body) => {
+    if (err) t.error(err)
+    t.equal(body.ua, 'tape')
     t.end()
   })
 })
